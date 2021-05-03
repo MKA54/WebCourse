6d584b3af4498e68478d0c24f0cb4a089c25708f
Mka91 <kiwogens@yandex.ru>
2021-05-03 18:48:17 +0700

    BootstrapTodoList

--- BootstrapTodoList/script.js
+++ BootstrapTodoList/script.js
@@ -6,15 +6,17 @@ $(document).ready(function () {
     var inputForm = $("#inputForm");
 
     $("#add_button").click(function () {
+        var text = nextTodoTextField.val().trim();
+
         inputForm.removeClass("was-validated");
 
-        if (inputForm[0].checkValidity() === false) {
+        if (inputForm[0].checkValidity() === false || text.length === 0) {
+            nextTodoTextField.val("");
+
             inputForm.addClass("was-validated");
 
             return;
         }
-
-        var text = nextTodoTextField.val().trim();
 
         function setViewMode() {
             listItem.html("<span class='text'></span><button type='button' class='mx-1 btn btn-warning edit_button'>Редактировать</button>" +
@@ -33,23 +35,26 @@ $(document).ready(function () {
             });
 
             listItem.find(".edit_button").click(function () {
-                listItem.html("<div class='row'><div class='d-inline-block'><input type='text' class='form-control indent edit_text' required/><div class='invalid-feedback'>Введите текст</div></div>" +
-                    "<div class='d-inline-block'><button type='button' class='btn btn-success save_button ml-2'>Сохранить</button> <button type='button' class='btn btn-danger cancel_button'>Отмена</button></div></div>");
+                listItem.html("<div class='row'><div class='d-inline-block'><input type='text' class='form-control edit_text' required/>" +
+                    "<div class='invalid-feedback'>Введите текст</div></div><div class='d-inline-block'><button type='button' " +
+                    "class='btn btn-success save_button ml-2'>Сохранить</button> <button type='button' class='btn btn-danger cancel_button'>Отмена</button></div></div>");
 
                 listItem.find(".edit_text").val(text);
 
                 listItem.find(".save_button").click(function () {
                     var listForm = $("#listForm");
 
+                    text = listItem.find(".edit_text").val().trim();
+
                     listForm.removeClass("was-validated");
 
-                    if (listForm[0].checkValidity() === false) {
+                    if (listForm[0].checkValidity() === false || text.length === 0) {
+                        listItem.find(".edit_text").val("");
+
                         listForm.addClass("was-validated");
 
                         return;
                     }
-
-                    text = listItem.find(".edit_text").val().trim();
 
                     setViewMode();
                 });
