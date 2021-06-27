53bb7f91c3215290773e085a9c17d6af9a7b782a
Mka91 <kiwogens@yandex.ru>
2021-06-27 19:14:59 +0700

    test

---
+++ test/script.js
@@ -0,0 +1,106 @@
+new Vue({
+    el: "#app",
+
+    data: {
+        checked: false,
+        picked: 3,
+        telephones: [
+            {
+                name: "Apple iPhone 12",
+                producer: "Apple",
+                releaseYear: 2020,
+                diagonal: "6,1",
+                producingCountry: "China",
+                memorySize: "128 Гб",
+                screenRefreshRate: "60 Гц",
+                NFC: src="bad.jpg",
+                eSIM: src="ok.jpg",
+                wirelessCharging: src="ok.jpg",
+                cost: "81 990 &#8381"
+            },
+            {
+                name: "Xiaomi Mi 11 Lite",
+                producer: "Xiaomi",
+                releaseYear: 2021,
+                diagonal: "6,55",
+                producingCountry: "China",
+                memorySize: "128 Гб",
+                screenRefreshRate: "60 Гц",
+                NFC: src="ok.jpg",
+                eSIM: src="ok.jpg",
+                wirelessCharging: src="bad.jpg",
+                cost: "27 490 &#8381"
+            },
+            {
+                name: "Samsung Galaxy A72",
+                producer: "Samsung",
+                releaseYear: 2021,
+                diagonal: "6,7",
+                producingCountry: "Vietnam",
+                memorySize: "128 Гб",
+                screenRefreshRate: "90 Гц",
+                NFC: src="ok.jpg",
+                eSIM: src="bad.jpg",
+                wirelessCharging: src="ok.jpg",
+                cost: "32 890 &#8381"
+            },
+            {
+                name: "Samsung Galaxy S21",
+                producer: "Samsung",
+                releaseYear: 2021,
+                diagonal: "6,2",
+                producingCountry: "Vietnam",
+                memorySize: "128 Гб",
+                screenRefreshRate: "120 Гц",
+                NFC: src="ok.jpg",
+                eSIM: src="ok.jpg",
+                wirelessCharging: src="ok.jpg",
+                cost: "58 790 &#8381"
+            },
+            {
+                name: "Apple iPhone Xr",
+                producer: "Apple",
+                releaseYear: 2018,
+                diagonal: "6,1",
+                producingCountry: "China",
+                memorySize: "64 Гб",
+                screenRefreshRate: "60 Гц",
+                NFC: src="ok.jpg",
+                eSIM: src="ok.jpg",
+                wirelessCharging: src="ok.jpg",
+                cost: "70 000 &#8381"
+            },
+            {
+                name: "Realme 8 Pro",
+                producer: "Realme",
+                releaseYear: 2021,
+                diagonal: "6,4",
+                producingCountry: "China",
+                memorySize: "128 Гб",
+                screenRefreshRate: "60 Гц",
+                NFC: src="ok.jpg",
+                eSIM: src="bad.jpg",
+                wirelessCharging: src="bad.jpg",
+                cost: "24 990 &#8381"
+            }
+        ]
+  // phonesCount
+    },
+
+    methods: {
+        downloadsPhones: function () {
+           var lastCells = document.querySelector("table tr");
+
+            var self = this;
+
+            for (var i = 0; i < this.telephones.length; i++) {
+                for (var name in this.telephones[i]){
+                    lastCells.find("td:last-child").text(self.telephones[i][name]);
+                    }
+
+                    //lastCells.text(this.telephones[i][name]);
+                }
+
+            }
+        }
+});
